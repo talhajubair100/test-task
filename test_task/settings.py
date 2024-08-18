2264bf02f923dbea1b41719a1f4d3f16e078721a
talhajubair100 <talhajubair100.bd@gmail.com>
2024-08-18 13:45:45 +0600

    Update events.html template to handle empty event list

--- test_task/settings.py
+++ test_task/settings.py
@@ -145,6 +145,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # email settings
 
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'yourhost.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'yourhost.com.au'
+EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = 'yourhost.com.au'
 
 
 
